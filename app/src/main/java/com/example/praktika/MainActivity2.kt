cd6e990d5b3263339f70ae13208d707538452ab6
Oleksii Fedir <fediroleksii57@gmail.com>
2023-02-14 12:04:51 +0300

    test
    test

---
+++ app/src/main/java/com/example/praktika/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.praktika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    fun onClickClose(view : View) {
+
+        finish()
+
+    }
+}
